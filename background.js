aa87cf608082bef61d45ef2b7946594e8ec98244
Nikola Mirilo <nikolamirilo@gmail.com>
2025-02-08 11:51:44 +0100

    updated background.js

--- background.js
+++ background.js
@@ -1,6 +1,3 @@
-// This file currently doesn't have specific functionality.
-// You can use it to manage background processes or messaging if needed.
-
 chrome.runtime.onInstalled.addListener(() => {
     console.log("XPath Finder Extension installed.");
 });
